7d3f894d11492beaa2a8d32be1f6d0d7fe06b0f7
Li-Qingyun <962537281@qq.com>
2025-01-13 21:21:02 +0800

    update script

--- scripts/florence-2-l_vis1024-lang2048_dota1-v2_b2x8xga2-100e.sh
+++ scripts/florence-2-l_vis1024-lang2048_dota1-v2_b2x8xga2-100e.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-TOTAL_BATCH_SIZE=${TOTAL_BATCH_SIZE:-32}
-NGPUS=16
-PER_RANK_BATCH_SIZE=$((TOTAL_BATCH_SIZE / NGPUS))
-RUN_NAME="florence-2-l_vis1024-lang2048_dota1-v2_b2x16-100e-zero2"
-
-export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-HOSTNAMES=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
-THEID=$(echo -e $HOSTNAMES | python3 -c "import sys;[sys.stdout.write(str(i)) for i,line in enumerate(next(sys.stdin).split(' ')) if line.strip() == '$(hostname)'.strip()]")
-echo MASTER_ADDR=$MASTER_ADDR
-echo HOSTNAMES=$HOSTNAMES
-echo SLURM_PROCID=$THEID
+RUN_NAME="florence-2-l_vis1024-lang2048_dota1-v2_b2x8xga2-100e-zero2"
 
 set -x
-PYTHONPATH="."$PYTHONPATH ACCELERATE_CPU_AFFINITY=1 torchrun --nnodes=2 --nproc-per-node=8 \
+PYTHONPATH="."$PYTHONPATH ACCELERATE_CPU_AFFINITY=1 torchrun --nnodes=1 --nproc-per-node=8 \
     --master_port 12968 --master_addr ${MASTER_ADDR} --node_rank ${THEID} \
     -m lmmrotate.train \
     --deepspeed ./lmmrotate/deepspeed_config/zero2.json \
@@ -25,9 +15,9 @@ PYTHONPATH="."$PYTHONPATH ACCELERATE_CPU_AFFINITY=1 torchrun --nnodes=2 --nproc-
     --attn_implementation "flash_attention_2" \
     --output_dir ./checkpoints/${RUN_NAME} \
     --num_train_epochs 100 \
-    --per_device_train_batch_size ${PER_RANK_BATCH_SIZE} \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --eval_strategy "no" \
     --eval_steps 200 \
     --save_strategy "steps" \
