13c737d600313cee7afb1fd48905a06e2b700b72
Li-Qingyun <962537281@qq.com>
2025-02-18 15:45:31 +0800

    update scripts

--- scripts/eval_standalone.sh
+++ scripts/eval_standalone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+NGPUS=${NGPUS:-1}
 SPLIT=${SPLIT:-"trainval test"}
 CKPT=$1
 OTHER_ARGS=${@:2}
 
-python -m lmmrotate.eval --model_ckpt_path ${CKPT} --split ${SPLIT} ${OTHER_ARGS}
+torchrun --nproc_per_node=${NGPUS} -m lmmrotate.eval --model_ckpt_path ${CKPT} --split ${SPLIT} ${OTHER_ARGS}
--- scripts/florence-2-l_vis1024-lang2048_dota1-v2_b2x4xga4-50e.sh
+++ scripts/florence-2-l_vis1024-lang2048_dota1-v2_b2x4xga4-50e.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-RUN_NAME="florence-2-l_vis1024-lang2048_dota1-v2_b2x8xga2-100e-zero2"
+RUN_NAME="florence-2-l_vis1024-lang2048_dota1-v2_b2x4xga4-50e-zero2"
 
 set -x
-PYTHONPATH="."$PYTHONPATH ACCELERATE_CPU_AFFINITY=1 torchrun --nnodes=1 --nproc-per-node=8 \
-    --master_port 12968 --master_addr ${MASTER_ADDR} --node_rank ${THEID} \
+PYTHONPATH="."$PYTHONPATH ACCELERATE_CPU_AFFINITY=1 torchrun --nnodes=1 --nproc-per-node=4 \
     -m lmmrotate.train \
     --deepspeed ./lmmrotate/deepspeed_config/zero2.json \
     --model_name_or_path 'microsoft/Florence-2-large' \
@@ -11,13 +10,13 @@ PYTHONPATH="."$PYTHONPATH ACCELERATE_CPU_AFFINITY=1 torchrun --nnodes=1 --nproc-
     --language_model_max_length 2048 \
     --data_path ./playground/data/florence-dota/florence_split_ss_dota_trainval_v2.json \
     --image_folder ./playground/data/split_ss_dota/trainval/images \
-    --bf16 True \
+    --fp16 True \
     --attn_implementation "flash_attention_2" \
     --output_dir ./checkpoints/${RUN_NAME} \
-    --num_train_epochs 100 \
+    --num_train_epochs 50 \
     --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 2 \
-    --gradient_accumulation_steps 2 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 4 \
     --eval_strategy "no" \
     --eval_steps 200 \
     --save_strategy "steps" \
@@ -27,7 +26,7 @@ PYTHONPATH="."$PYTHONPATH ACCELERATE_CPU_AFFINITY=1 torchrun --nnodes=1 --nproc-
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --gradient_checkpointing True \
+    --gradient_checkpointing False \
     --dataloader_num_workers 4 \
     --report_to "tensorboard" \
     --run_name ${RUN_NAME}
